b691440ac46c267166d5790a79f62f445ac38d68
maryaalshameri <marynaalshameri@gmail.com>
2025-09-08 22:35:15 +0300

    feat: Create admin panel views

---
+++ resources/views/admin/products.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title','Admin - Products')
+
+@section('content')
+<div class="container py-4">
+    <h2>Products</h2>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Image</th><th>Name</th><th>Category</th><th>Price</th><th>On Sale</th>
+            </tr>
+        </thead>
+        <tbody>
+        @forelse($products as $p)
+            <tr>
+                <td><img src="{{ asset($p->image) }}" alt="" width="60"></td>
+                <td>{{ $p->name }}</td>
+                <td>{{ $p->category->name ?? '-' }}</td>
+                <td>${{ number_format($p->price,2) }}</td>
+                <td>{{ $p->on_sale ? 'Yes' : 'No' }}</td>
+            </tr>
+        @empty
+            <tr><td colspan="5">No products found.</td></tr>
+        @endforelse
+        </tbody>
+    </table>
+
+    {{ $products->links() }}
+</div>
+@endsection
