0540776bc3317a3e7a71c5833b1947dff655d04f
maryaalshameri <marynaalshameri@gmail.com>
2025-09-09 21:35:35 +0300

    feat: Define 'access-admin-panel' gate

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,29 @@ use App\Http\Controllers\StoreControllar;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminController;
-Route::get('/',[ShopControllar::class,'index']);
+// Route::get('/',[ShopControllar::class,'index']);
 // Route::get('/products',[StoreControllar::class,'products']);
 
 // // Route::get('/product/{id}', [StoreControllar::class, 'productDetails']);
-Route::get('/about', [StoreControllar::class, 'about']);
-Route::get('/contact', [StoreControllar::class, 'contact']);
+// Route::get('/about', [StoreControllar::class, 'about']);
+// Route::get('/contact', [StoreControllar::class, 'contact']);
 
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); 
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-Route::get('/products/{product}/edit', [ProductController::class,'edit'])->name('products.edit');
-Route::put('/products/{product}', [ProductController::class,'update'])->name('products.update');
-Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
+// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); 
+// Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+// Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+// Route::get('/products/{product}/edit', [ProductController::class,'edit'])->name('products.edit');
+// Route::put('/products/{product}', [ProductController::class,'update'])->name('products.update');
+// Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+// Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
 // // Route::resource('products', ProductController::class);
-Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-    Route::get('/products', [AdminController::class, 'products'])->name('admin.products');
-    Route::get('/categories', [AdminController::class, 'categories'])->name('admin.categories');
-});
+// Route::prefix('admin')->group(function () {
+//     Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+//     Route::get('/products', [AdminController::class, 'products'])->name('admin.products');
+//     Route::get('/categories', [AdminController::class, 'categories'])->name('admin.categories');
+// });
 
 
 Route::get('/', function () {
@@ -43,5 +43,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'can:access-admin-panel'])
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    });
 
 require __DIR__.'/auth.php';
